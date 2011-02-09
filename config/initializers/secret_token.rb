952bd90b4804b5cdcdfb05e337f0538df96132e6
Hiroki Yoshioka <irohiroki@gmail.com>
2011-02-10 01:13:34 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendtoMobile::Application.config.secret_token = 'f611adad6c9c3059c8ebef29b208d0adfa3b595c0044e6ebe3c7c01688dd80aec6aa45748e6b12f8a34d878957585dc00e48e857ddacbece992415d1334f0698'
