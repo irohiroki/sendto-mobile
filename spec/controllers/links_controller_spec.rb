6b37acf85d8f1e3f2c97c082fb69d4d790190228
Hiroki Yoshioka <irohiroki@gmail.com>
2011-03-15 10:22:29 +0900

    refactor the links controller spec.

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe LinksController do
-  let(:u){ create_user }
+  before(:all) { create_user }
+
+  let(:l){ mock_model(Link).as_null_object }
+  let(:u){ User.first }
+
+  after(:all) { User.delete_all }
 
   before do
     @controller.stub(:current_user).and_return(u)
@@ -9,41 +14,58 @@ describe LinksController do
   end
 
   describe "GET send" do
-    it "should be successful" do
+    def do_send
       get 'send_', 'link' => 'http://example.com', 'title' => 'example'
-      response.should be_success
+    end
+
+    shared_examples_for "updating and success" do
+      it 'updates the link' do
+        l.should_receive(:update_attributes).with(:uri => 'http://example.com', :title => 'example')
+        do_send
+      end
+
+      specify do
+        do_send
+        response.should be_success
+      end
     end
 
     context "if user has already a link" do
-      before do
-        u.create_link(:uri => 'http://a', :title => 'a')
-        get 'send_', 'link' => 'http://example.com', 'title' => 'example'
-      end
+      before { u.should_receive(:link).and_return(l) }
 
-      it 'updates the link' do
-        u.link.uri.should eq 'http://example.com'
-      end
-
-      it 'updates the title' do
-        u.link.title.should eq 'example'
-      end
+      it_behaves_like "updating and success"
     end
 
     context "if user has no link yet" do
-      pending
+      before do
+        u.stub(:link).and_return(nil)
+        u.should_receive(:build_link).and_return(l)
+      end
+
+      it_behaves_like "updating and success"
     end
   end
 
   describe "GET receive" do
-    before { u.create_link :uri => 'http://foo.com' }
+    context "if user has already a link" do
+      before do
+        u.stub(:link).and_return(l)
+        l.stub(:uri).and_return('http://foo.com')
+      end
 
-    it "redirects to the saved link" do
-      get 'receive_'
-      response.should redirect_to('http://foo.com')
+      it "redirects to the saved link" do
+        get 'receive_'
+        response.should redirect_to('http://foo.com')
+      end
     end
 
-    context "after an unsuccessful send" do
-      pending
+    context "if user has no link yet" do
+      before { u.stub(:link).and_return(nil) }
+
+      it "renders the default template" do
+        get 'receive_'
+        response.should render_template('links/receive_')
+      end
     end
   end
 end
