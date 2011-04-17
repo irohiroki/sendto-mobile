4aec4d643b3fd26f33f05b485c379e0a11229f32
Hiroki Yoshioka <irohiroki@gmail.com>
2011-04-17 14:58:48 +0900

    refine the pop-up window.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,7 +9,6 @@ class LinksController < ApplicationController
   def send_
     @link = current_user.link || current_user.build_link
     @link.update_attributes(:uri => params[:link], :title => params[:title])
-    render :layout => 'send'
   end
 
   def receive_
